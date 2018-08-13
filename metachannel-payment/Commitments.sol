4ef1401be54d054612989e4da60f8c87996e345e
Li Xuanji <xuanji@gmail.com>
2018-08-13 12:19:13 -0400

    WIP

---
+++ metachannel-payment/Commitments.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.17;
+
+import "./Registry.sol";
+import "../mvsc/cf-instantiated-objects/RootNonce.sol";
+import "../mvsc/cf-instantiated-objects/Payment.sol";
+
+
+contract Commitments {
+
+  function proxyCommitment(
+    address registryA,
+    bytes32 rootCA,
+    bytes32 paymentCA,
+    uint256 rootExpected
+  ) public {
+    Registry registry = Registry(registryA);
+    address rootA = registry.resolver(rootCA);
+    RootNonce rootCO = RootNonce(rootA);
+    require(rootCO.finalizedNonce() == rootExpected);
+    address paymentA = registry.resolver(paymentCA);
+    Payment paymentCO = Payment(paymentA);
+    require(paymentCO.isFinal());
+    for (uint256 i = 0; i < paymentCO.length; i++) {
+      paymentCO.owners(i).transfer(paymentCO.balance(i));
+    }
+  }
+
+}
---
+++ metachannel-payment/MetachannelPayment.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.17;
+
+uint256 TIMEOUT = 100;
+
+contract MetachannelPayment {
+
+  address intermediary;
+  address[2] public owners;
+  uint256 public balance; // balance of owners[0]
+
+  bool public isFinal;
+  uint256 public finalizesAt;
+
+  uint256 nonce = 0;
+
+  constructor(address[2] _owners) public {
+    owners = _owners;
+    finalizesAt = block.number + TIMEOUT;
+  }
+
+  function removeIntermediary(
+    uint8[] v,
+    bytes32[] r,
+    bytes32[] s
+  ) public {
+    bytes32 digest = keccak256(abi.encode(intermediary, owners));
+    require(intermediary == ecrecover(digest, v[i], r[i], s[i]));
+    intermediary = 0;
+  }
+
+  function update(
+    uint8[] v,
+    bytes32[] r,
+    bytes32[] s,
+    uint256 _balance,
+    uint256 _nonce
+  ) public {
+
+    require(!isFinal);
+    require(_nonce > nonce);
+    require(intermediary == 0);
+
+    bytes32 digest = keccak256(_balances, _nonce);
+
+    for (uint256 i = 0; i < owners.length; i++) {
+      require(owners[i] == ecrecover(digest, v[i], r[i], s[i]));
+    }
+
+    balance = _balance;
+
+    nonce = _nonce;
+    finalizesAt = block.number + 10;
+  }
+
+  function finalize () public {
+    require(block.number >= finalizesAt);
+    isFinal = true;
+  }
+
+}
